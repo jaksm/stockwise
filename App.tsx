297f1ca7759394a61ea75989e6a6628cecaebe6f
Jaksa <jaksa.malisic@gmail.com>
2025-01-02 16:44:36 +0100

    chore: setup react-query

--- App.tsx
+++ App.tsx
@@ -4,13 +4,19 @@ import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 
+import {QueryClient, QueryClientProvider} from 'react-query';
+
 import {RootStack} from './app/RootStack';
+
+const queryClient = new QueryClient();
 
 function App(): React.JSX.Element {
   return (
-    <NavigationContainer>
-      <RootStack />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import type {RootStackNavigation} from '../RootStack';
+import {useGetSymbols} from '../api/queries/useGetSymbols';
 import {Button} from '../components/ui/Button';
 import {Flex} from '../components/ui/Flex';
+import {Label} from '../components/ui/typography';
 
 type HomeScreenProps = {
   navigation: RootStackNavigation;
 };
 
 export function Home({navigation}: HomeScreenProps) {
+  const {data} = useGetSymbols(['AAPL']);
+
   return (
     <Flex gap="3">
+      <Label>{JSON.stringify(data, null, 2)}</Label>
       <Button onPress={() => navigation.navigate('AssetDetails')}>
         Go to Asset Details
       </Button>
